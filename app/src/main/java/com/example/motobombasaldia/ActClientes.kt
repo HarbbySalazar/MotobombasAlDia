1ace667526540a019f4c46a6c08953ed5a5fc17c
hospicovid <harbbysalazar@gmail.com>
2022-11-06 14:13:19 -0500

    primer comit

---
+++ app/src/main/java/com/example/motobombasaldia/ActClientes.kt
@@ -0,0 +1,19 @@
+package com.example.motobombasaldia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class ActClientes : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+      /*  cmbIdentificaciones = findViewById(R.id.cbTipoUsuario)
+        val adaptador = ArrayAdapter.createFromResource(this, R.array.tiposIdentificacion,android.R.layout.activity_list_item)
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        cmbIdentificaciones.adapter = adaptador*/
+
+    }
+}
